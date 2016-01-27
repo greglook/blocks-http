a1ce423e286fe74c0dac23ec6cf4a88371204123
Greg Look <greg@greg-look.net>
2016-01-26 20:28:21 -0800

    Add do-request macro to simplify API calls.

--- src/blocks/store/http/client.clj
+++ src/blocks/store/http/client.clj
@@ -16,6 +16,32 @@
   (str server-url (multihash/base58 id)))
 
 
+(defmacro ^:private do-request
+  [request & body]
+  (let [[success body] (if (and (< 1 (count body))
+                                (integer? (first body)))
+                         [(first body) (rest body)]
+                         [200 body])]
+    `(let [response# ~request]
+       (condp = (:status response#)
+         ; Successful response.
+         ~success
+         (let [~'response response#]
+           ~@body)
+
+         ; Not found.
+         404 nil
+
+         ; Some other error status.
+         (throw (ex-info (str "Unsuccessful blocks-http response: "
+                              (:status response#) " - "
+                              (:error (:body response#) "--"))
+                         {:expected ~success
+                          :status (:status response#)
+                          :headers (:headers response#)
+                          :body (:body response#)}))))))
+
+
 
 ;; ## HTTP Block Client
 
@@ -27,79 +53,59 @@
   (-stat
     [this id]
     (when id
-      (let [response (http/head (block-url server-url id)
-                                {:throw-exceptions false})]
-        (condp = (:status response)
-          ; Successful response.
-          200 (assoc (util/header-stats (:headers response))
-                     :id id)
-
-          ; Block wasn't found.
-          404 nil
-
-          ; Some other error status.
-          (throw (ex-info (str "Unsuccessful blocks-http response: "
-                               (:status response) " - "
-                               (:error (:body response) "--"))
-                          {:status (:status response)
-                           :body (:body response)}))))))
+      (do-request (http/head (block-url server-url id)
+                             {:throw-exceptions false})
+        (-> (:headers response)
+            (util/header-stats)
+            (assoc :id id)))))
 
 
   (-list
     [this opts]
-    (let [response (http/get server-url
-                             {:query-params opts
-                              :throw-exceptions false
-                              ;:debug true
-                              :as :json})]
-      (when (not= 200 (:status response))
-        (throw (ex-info (str "Unsuccessful blocks-http response: "
-                             (:status response) " - "
-                             (:error (:body response) "--"))
-                        {:status (:status response)
-                         :body (:body response)})))
+    (do-request (http/get server-url
+                          {:query-params opts
+                           :throw-exceptions false
+                           :accept :edn
+                           :debug true
+                           :as :edn})
       ; TODO: lazy seq a-la s3
       ; TODO: parse :id and :stored-at
-      (:data (:body response))))
+      (->> (:body response)
+           (:data)
+           (map #(-> % (update :id multihash/decode)
+                       (update :stored-at util/parse-date))))))
 
 
   (-get
     [this id]
     (when-let [stats (.-stat this id)]
-      ; TODO: return a lazy block instead
-      (let [response (http/get (block-url server-url id)
-                               {:throw-exceptions false})]
-        (condp = (:status response)
-          200 (block/with-stats
-                (block/read! (:body response))
-                (util/header-stats (:headers response)))
-
-          ; Block wasn't found.
-          404 nil
-
-          ; Some other status.
-          (throw (ex-info (str "Unsuccessful blocks-http response: "
-                               (:status response) " - "
-                               (:error (:body response) "--"))
-                          {:status (:status response)
-                           :body (:body response)}))))))
+      ; TODO: return a lazy block instead?
+      (do-request (http/get (block-url server-url id)
+                            {:throw-exceptions false})
+        (block/with-stats
+          (block/read! (:body response))
+          (util/header-stats (:headers response))))))
 
 
   (-put!
     [this block]
     (when block
-      (throw (UnsupportedOperationException. "NYI"))
-      ; TODO: store block content
-      (let [response (http/put (block-url server-url (:id block)))]
-        ; TODO: PUT /:id
-        )))
+      (do-request (http/put (block-url server-url (:id block))
+                            {:body (block/open block)
+                             :length (:size block)
+                             :content-type "application/octet-stream"
+                             :throw-exceptions false})
+        204
+        (block/with-stats
+          block
+          (dissoc (util/header-stats (:headers response)) :size)))))
 
 
   (-delete!
     [this id]
     (when id
       (throw (UnsupportedOperationException. "NYI"))
-      (let [response (http/delete (block-url server-url id))]
+      (do-request (http/delete (block-url server-url id))
         ; TODO: parse response for successful deletion
         ))))
 
